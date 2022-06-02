e8650903efa6fd6e54ecc116d6b0f5bc29f31df3
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-01 19:45:30 -0700

    needs to be solved in python

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,28 @@
-exports.caesarCipher = function() {
+exports.caesarCipher = function(str,num) {
 
-};
+        let regex = /[^a-z]/gi
+        let holdArr = [];
+        let answerArr = [];
+      
+        for (let i=0;i<str.length;i++) {
+          if (str[i].match(regex) != null) {
+            holdArr.push(str[i]);
+            answerArr.push(str[i]);
+          } else {
+            holdArr.push(str[i].charCodeAt()+num);
+            if (holdArr[i] > 122 || (holdArr[i] > 90 && holdArr[i] < 97)) {
+              holdArr[i] -= 26;
+            }
+            answerArr.push(String.fromCharCode(holdArr[i]));
+          }
+        }
+      
+        str = answerArr.join("");
+        return str; 
+      
+      }
+      
+// caesarCipher('coderBYTE', 2)
+// output: eqfgtDAVG    
+
+// need to adjust code to be able to handle negative numbers
--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,7 @@
 def caesar_cipher(string, shift_amount):
+
+# list(string)
+
+# for x in enumerate(string)
+
+# - % operators
